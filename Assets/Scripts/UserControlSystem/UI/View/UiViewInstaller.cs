e1ed1a86a1a4f88c5c853a52743df963d3ab6cc1
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2021-10-14 16:24:24 +0600

    попытка исправить ошибку

--- Assets/Scripts/UserControlSystem/UI/View/UiViewInstaller.cs
+++ Assets/Scripts/UserControlSystem/UI/View/UiViewInstaller.cs
@@ -4,9 +4,6 @@ public class UiViewInstaller : MonoInstaller
 {
 	public override void InstallBindings()
 	{
-		Container
-		.Bind<BottomCenterView>()
-		.FromComponentInHierarchy()
-		.AsSingle();
+		Container.Bind<BottomCenterView>().FromComponentInHierarchy().AsSingle();
 	}
 }
