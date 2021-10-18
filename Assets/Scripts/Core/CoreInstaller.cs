75d17604038a52cafdf32224df9e8302fc6c2eda
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2021-10-18 16:32:00 +0600

    Выполнил ДЗ №2 к лекции № 7

--- Assets/Scripts/Core/CoreInstaller.cs
+++ Assets/Scripts/Core/CoreInstaller.cs
@@ -1,9 +1,12 @@
+using UnityEngine;
 using Zenject;
 
 public class CoreInstaller : MonoInstaller
 {
+	[SerializeField] private IGameStatus _gameStatus;
 	public override void InstallBindings()
 	{
 		Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+		Container.Bind<IGameStatus>().FromInstance(_gameStatus);
 	}
 }
