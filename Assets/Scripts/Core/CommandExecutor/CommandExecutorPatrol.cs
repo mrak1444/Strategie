02e24277ab1004c3454d33cedea56e2543eafe18
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2021-10-18 15:40:25 +0600

    Выполнил ДЗ №1 к лекции № 7

--- Assets/Scripts/Core/CommandExecutor/CommandExecutorPatrol.cs
+++ Assets/Scripts/Core/CommandExecutor/CommandExecutorPatrol.cs
@@ -1,17 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CommandExecutorPatrol : CommandExecutorBase<IPatrolCommand>
 {
+    [SerializeField] private UnitMovementStop _stop;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private CommandExecutorStop _stopCommandExecutor;
+
     public override async Task ExecuteSpecificCommand(IPatrolCommand command)
     {
-        await Task.Run(() => Patrol(command));
-    }
+        var point1 = command.From;
+        var point2 = command.To;
+        while (true)
+        {
+            GetComponent<NavMeshAgent>().destination = point2;
+            _animator
+                    .SetTrigger(Animator
+                            .StringToHash("Walk"));
+            _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+            try
+            {
+                await _stop.WithCancellation(_stopCommandExecutor.CancellationTokenSource.Token);
+            }
+            catch
+            {
+                GetComponent<NavMeshAgent>().isStopped = true;
+                GetComponent<NavMeshAgent>().ResetPath();
+                break;
+            }
 
-    private void Patrol(IPatrolCommand command)
-    {
-        Debug.Log($"{name} moves from {transform.position} to {command.To}");
+            var temp = point1;
+            point1 = point2;
+            point2 = temp;
+        }
+        _stopCommandExecutor.CancellationTokenSource = null;
+        _animator.SetTrigger(Animator.StringToHash("Idle"));
     }
 }
